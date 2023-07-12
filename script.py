73f720921adbf23cd03dc56bc070709f239e209d
ashwabu <abdulazizredi90@gmail.com>
2023-07-12 18:07:12 +0300

    face detector

--- script.py
+++ script.py
@@ -1,10 +1,15 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('photo.jpg')
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_image,scaleFactor=1.5,minNeighbors=5)
+
+print(type(faces))
+print(faces)
 
 cv2.imshow("Gray",gray_image)
 cv2.waitKey(0)
