25a73fa77bc6ea469a78efdc1fa0a8a06cc6033b
ashwabu <abdulazizredi90@gmail.com>
2023-07-12 18:22:17 +0300

    testing with complex images

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('photo.jpg')
+img = cv2.imread('news.jpg')
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
